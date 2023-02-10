488e32463bff7e4963977586a3c9f4b9d0e97414
anthony3m <124469271+anthony3m@users.noreply.github.com>
2023-02-09 20:55:23 -0500

    Add files via upload

---
+++ 1-Two-Sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = {}
+
+        for i, val in enumerate(nums):
+            diff = target - val
+
+            if diff in hashMap:
+                return [hashMap[diff], i]
+            hashMap[val] = i
