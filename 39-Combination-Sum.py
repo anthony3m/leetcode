6d9381bc20b1463869b731f46b941d5bff26bdb7
anthony3m <124469271+anthony3m@users.noreply.github.com>
2023-03-19 23:52:34 -0400

    Update 39-Combination-Sum.py

--- 39-Combination-Sum.py
+++ 39-Combination-Sum.py
@@ -1,18 +1,18 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
+        def dfs(i, path):
+            if sum(path) == target:
+                res.append(path[:])
+                return 
+			
+            if sum(path) > target:
+                return 
+			
+            for x in range(i, len(candidates)):
+                path.append(candidates[x])
+                dfs(x, path)
+                path.pop()
 
-        def dfs(i, cur, total):
-            if total == target:
-                res.append(cur.copy())
-                return
-            if i >= len(candidates) or total > target:
-                return
-
-            cur.append(candidates[i])
-            dfs(i, cur, total + candidates[i])
-            cur.pop()
-            dfs(i + 1, cur, total)
-
-        dfs(0, [], 0)
+        dfs(0, [])
         return res
